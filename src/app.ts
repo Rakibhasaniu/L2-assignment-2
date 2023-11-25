5f3220ff1a82fd274eca4ff0489d26552b5aae23
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2023-11-25 19:42:34 +0600

    app initialized

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-const app: Application = express();
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
+app.get('/', (req: Request, res: Response) => {
+  res.send('server is running!')
+})
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("server is running!");
-});
-
-export default app;
+export default app
