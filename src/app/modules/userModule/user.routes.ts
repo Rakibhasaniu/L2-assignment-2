d67b4e400f69f3da541a48419d9f20bd58bd3923
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2023-11-25 20:29:02 +0600

    initially completed

--- src/app/modules/userModule/user.routes.ts
+++ src/app/modules/userModule/user.routes.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { userControllers } from './user.controller'
+import { ordersController } from '../orderModule/orders.controller'
 
 const router = express.Router()
 
@@ -8,5 +9,9 @@ router.get('/', userControllers.getAllUsers)
 router.get('/:userId', userControllers.getOneUser)
 router.put('/:userId', userControllers.updateOneUser)
 router.delete('/:userId', userControllers.deleteOneUser)
+
+router.put('/:userId/orders', ordersController.addNewProduct)
+router.get('/:userId/orders', ordersController.getAllProduct)
+router.get('/:userId/orders/total-price', ordersController.totalpriceCount)
 
 export const userRoute = router
